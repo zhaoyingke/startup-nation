b142af53d7b337833149e94f2d152767e5fe7e2b
Jules Bonnard <julesbonnard@gmail.com>
2019-03-26 10:18:18 +0100

    init store

--- src/store.js
+++ src/store.js
@@ -5,7 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    money: 0,
+    marketShare: 0, // Can be between 0 and 1
+    reputationPoints: 0,
+    investorPoints: 0,
+    mode: null, // Can be easy, hard, or null
+    autoFinance: null // Can be between 0 and 1, or null
   },
   mutations: {
 
