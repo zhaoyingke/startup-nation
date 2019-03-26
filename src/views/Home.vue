9519edaed0fc494d9aeb6eeba7df0b40d91e7a16
ZYK <yingke.zhao@sciencespo.fr>
2019-03-26 16:30:15 +0100

    add logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <img alt="Startup logo" src="../assets/logo.png">
     <p>Bienvenue dans la Startup Nation ! Êtes-vous la prochaine licorne française ?</p>
     <router-link
       tag="button"
